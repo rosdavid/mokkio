35c7bb74f696942bbf8eb0a0cf282915db767c30
David Ros <davidros.dev@proton.me>
2025-10-25 01:17:42 +0200

    feat: agregar Analytics y SpeedInsights para mejorar el seguimiento y rendimiento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +24,8 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
