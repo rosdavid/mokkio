cb44fc442a87907fc7c84bb2e83b5918e3d66b02
David Ros <davidros.dev@proton.me>
2025-11-10 17:20:23 +0100

    feat: unificar comillas en las exportaciones de hooks y agregar nuevos hooks de mockup

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,10 +1,14 @@
 // Authentication hooks
-export { useAuth } from './auth/useAuth';
+export { useAuth } from "./auth/useAuth";
 
 // UI hooks
-export { useIsMobile } from './ui/use-mobile';
-export { useCookieConsent } from './ui/useCookieConsent';
+export { useIsMobile } from "./ui/use-mobile";
+export { useCookieConsent } from "./ui/useCookieConsent";
 
 // Mockup hooks
-export { usePresence } from './mockup/usePresence';
-export { useSectionVisible } from './mockup/useSectionVisible';
+export { usePresence } from "./mockup/usePresence";
+export { useSectionVisible } from "./mockup/useSectionVisible";
+export { useSnapGuides } from "./mockup/useSnapGuides";
+export { useDragAndDrop } from "./mockup/useDragAndDrop";
+export { useTextOverlay } from "./mockup/useTextOverlay";
+export { useBranding } from "./mockup/useBranding";
